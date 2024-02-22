71ea4e48deb82b90ef46068fb2e5675bc7163885
Jake Strasler <straslerj@msoe.edu>
2024-02-21 18:11:29 -0600

    App working locally

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # csc5201-lab-8
 CSC 5201 Microservices and Cloud Computing Lab 8
+
+
+## User Management API Instruction Manual
+
+### Introduction
+This API allows you to manage a list of users, including adding users with their name, location, and their distance to Milwaukee, WI. Users can also be removed from the list.
+
+### Base URL
+The base URL for the API is `http://127.0.0.1:5000/`.
+
+### Endpoints
+
+#### 1. Greeting
+- **Endpoint:** `/`
+- **Method:** `GET`
+- **Description:** Retrieves greeting, instructions.
+- **Example:** `http://127.0.0.1:5000/`
+- `curl http://127.0.0.1:5000/`
+
+#### 2. List all users
+- **Endpoint:** `/user`
+- **Method:** `GET`
+- **Description:** Retrieves a list of all users with their details.
+- **Example:** `http://127.0.0.1:5000/user`
+- `curl http://127.0.0.1:5000/user`
+
+#### 3. Add a new user
+- **Endpoint:** `/user`
+- **Method:** `POST`
+- **Description:** Adds a new user to the list.
+- **Request Body:**
+  ```json
+  {
+    "name": "John Doe",
+    "location": "City, State USA"
+  }
+- `curl -X POST -H "Content-Type: application/json" -d '{"name": "John Doe", "location": "Detroit, MI USA"}' http://127.0.0.1:5000/user`
+
+#### 4. Remove a user
+- **Endpoint:** `/user/<user_id>`
+- **Method:** `DELETE`
+- **Description:** Removes a user from the list by providing their `user_id`
+- **Example:** `http://127.0.0.1:5000/user/1`
+- `curl -X DELETE http://127.0.0.1:5000/user/<user_id>`
---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+from service import UserService
+from geopy.geocoders import Nominatim
+
+from geopy.distance import geodesic
+
+
+app = Flask(__name__)
+
+
+@app.after_request
+def add_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = (
+        "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+    )
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS"
+    return response
+
+
+@app.route("/")
+def hello():
+    return (
+        "Welcome to this application!\n"
+        "To interact with the user service, you can use the following endpoints:\n"
+        "1. GET /user - Retrieve a list of users\n"
+        "2. POST /user - Create a new user. Provide JSON data with 'name' and 'location'\n"
+        "   Example: {'name': 'John Doe', 'location': 'Detroit, MI USA'}\n"
+        "3. DELETE /user/<user_id> - Delete a user by specifying the user's ID\n"
+    )
+
+
+@app.route("/user", methods=["GET"])
+def list_users():
+    return jsonify(UserService().list_users())
+
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    user_data = {
+        "name": data["name"],
+        "location": data["location"],
+        "distance_to_milwaukee": calculate_distance(data["location"]),
+    }
+    return jsonify(UserService().create_user(user_data))
+
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    return jsonify(UserService().delete_user(user_id))
+
+
+def calculate_distance(hometown):
+    geolocator = Nominatim(user_agent="distance_calculator")
+    milwaukee_location = geolocator.geocode("Milwaukee, WI, USA")
+
+    if milwaukee_location is not None:
+        user_location = geolocator.geocode(hometown)
+        if user_location is not None:
+            distance_miles = geodesic(
+                (user_location.latitude, user_location.longitude),
+                (milwaukee_location.latitude, milwaukee_location.longitude),
+            ).miles
+            return round(distance_miles, 2)
+
+    return None
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
